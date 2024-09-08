d4794a1aaab0b381127cc89e883b0c2317d58eb2
Nikita Belenkov <56576246+nik-belenkov@users.noreply.github.com>
2024-09-08 14:03:08 +0700

    Add files via upload

---
+++ SQL_Day00/src/ex06/day00_ex06.sql
@@ -0,0 +1,12 @@
+SELECT
+    (SELECT name FROM person
+    WHERE id = person_id) AS NAME,
+    (CASE 
+        WHEN (SELECT name FROM person WHERE id = person_id) = 'Denis'
+        THEN TRUE
+        ELSE FALSE
+    END
+    ) AS CHECK_NAME
+FROM person_order
+WHERE (menu_id = 13 OR menu_id = 14 OR menu_id = 18) 
+AND order_date = '2022-01-07';
